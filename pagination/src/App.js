cbd32beb1ca9211c9ffa2d593ea51ff42098ab45
Angelina <stefan.m@pannovate.com>
2023-02-26 19:09:51 +0100

    small changes

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
-import { Navbar, Nav, Table, Row, Col, Container, Form } from "react-bootstrap";
+import { Navbar, Nav, Table, Row, Col, Container, Form, OverlayTrigger, Tooltip, Carousel } from "react-bootstrap";
 
 function useDebounce(fn, delay) {
   const [timer, setTimer] = useState(null);
@@ -49,7 +49,6 @@ function App() {
     queryParams.set("page", newPage);
     navigate(`?${queryParams.toString()}`);
   };
-
   const handlePerPageChange = (newPerPage) => {
     queryParams.set("perPage", newPerPage);
     navigate(`?${queryParams.toString()}`);
@@ -65,7 +64,14 @@ function App() {
   if (error) {
     return <div>{error}</div>;
   }
+  let tips = [];
+  
   console.log(data);
+  for(let i=0; i<data.length; i++) {
+    tips.push(data[i].brewers_tips);
+    
+  }
+  console.log(tips);
   return ( <>
   <Container fluid>
     <Row>
@@ -93,6 +99,23 @@ function App() {
     </Row>
     <Row>
       <Col md={8} sm={4}>
+    <Carousel>
+    {tips.map((tip, index) => (
+      
+      <Carousel.Item style={{backgroundColor: '#D0B8A8'}} key={index}>
+        <div style={{height: '200px'}}>
+
+        </div>
+        <Carousel.Caption>
+          <h3>Beer tip {index +1}</h3>
+          <p>{tip}</p>
+        </Carousel.Caption>
+      </Carousel.Item>))}
+    </Carousel>
+    </Col>
+    </Row>
+    <Row>
+      <Col md={8} sm={4}>
       <Table striped bordered hover size="sm" className="text-center">
         <thead>
           <tr>
@@ -101,12 +124,17 @@ function App() {
             <th>Name</th>
             <th>TagLine</th>
             <th>First Brewed</th>
-            <th>Description</th>
           </tr>
         </thead>
         <tbody>
           {data.map((item) => (
-            <tr key={item.id}>
+
+        <OverlayTrigger
+          placement="bottom"
+          delay={{ show: 250, hide: 400 }}
+          overlay={ <Tooltip  style={{backgroundColor: '#D0B8A8'}}>{item.description}</Tooltip>}
+          key={item.id}>
+            <tr>
               <td>{item.id}</td>
             <td>
               <img src={item.image_url} alt={item.name} style={{width: '50px', height: '100px' }}/>
@@ -114,14 +142,23 @@ function App() {
             <td>{item.name}</td>
             <td>{item.tagline}</td>
             <td>{item.first_brewed}</td>
-            <td>{item.description}</td>
             </tr>
+          </OverlayTrigger>
           ))}
         </tbody>
       </Table>
       </Col>
     </Row>
-    </Container></>
+    </Container>
+    <footer className="mt-auto col-md-8 col-sm-4">
+      <Container fluid>
+        <Row>
+          <Col className="text-center">
+            <p>&copy; {new Date().getFullYear()} Beereact</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer></>
   );
 }
 
